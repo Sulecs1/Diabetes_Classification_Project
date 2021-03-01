dc0aa50a44aa44e0bd32fb8b1a086d566c236537
Sulecs1 <sule.akcay3@ogr.sakarya.edu.tr>
2021-03-01 19:23:19 +0300

    gerekli işlemler ve incelemeler  yapıldı

--- Diabetes_Classification.py
+++ Diabetes_Classification.py
@@ -23,6 +23,8 @@ import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz, export_text
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import LocalOutlierFactor
 import missingno as msno
 import pickle
 import pydotplus
@@ -193,6 +195,140 @@ def check_plot(dataframe):
               plt.show()
 
 check_plot(df)
+
+clf = LocalOutlierFactor(n_neighbors = 20, contamination = 0.1)
+clf.fit_predict(df)
+df_scores = clf.negative_outlier_factor_
+scores = pd.DataFrame(np.sort(df_scores))
+scores.plot(stacked=True, xlim=[0, 20], style='.-')
+plt.show()
+
+esik_deger = np.sort(df_scores)[5] #eşik değerimiz
+df[df_scores < esik_deger] #eşik değerine göre seçim aykırıları seçtik yaptık
+df[df_scores < esik_deger].shape #sayısı
+df.describe().T
+#indeksleri tutma amacımız indekse göre kolay işlem yapmak
+df[df_scores < esik_deger].index
+#hepsini silmek istersek
+df.drop(axis=0, labels=df[df_scores < esik_deger].index)
+df = df.drop(axis=0, labels=df[df_scores < esik_deger].index)
+
+
+#kişinin akrabalarının diabet olma olasılığını 0-1 arasına çektik
+transformer = MinMaxScaler()
+df["DiabetesPedigreeFunction"] = transformer.fit_transform(df[["DiabetesPedigreeFunction"]])
+df.head()
+
+# 140 altı normal
+# 140-199 gizli şeker
+# 200 ve üzeri diyabet
+df["Insulin_Category"] = pd.cut(x=df["Insulin"],
+                           bins=[0, 140, 200, df["Insulin"].max()],
+                           labels=["Normal", "Gizli_Şeker", "Diyabet"])
+
+df["Insulin_Category"] = df["Insulin_Category"].fillna("Normal")
+df = one_hot_encoder(df, ["Insulin_Category"], drop_first=True)
+df = df.drop("Insulin", axis=1)
+df.head()
+
+#yukarıdaki target_summary_with_num,check_comp tekrar incele(182)
+
+y = df["Outcome"]
+X = df.drop(["Outcome"], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+
+cart_model = DecisionTreeClassifier(random_state=17).fit(X_train, y_train) #train veri setine göre fit ettik
+
+# train hatası
+y_pred = cart_model.predict(X_train) #train setinin bağımsız değişkenlerini yerine koyarak bağımlı değişkenlerini tahmin ettim
+y_prob = cart_model.predict_proba(X_train)[:, 1]
+print(classification_report(y_train, y_pred))
+roc_auc_score(y_train, y_prob) #1.0 overfit
+
+# test hatası
+y_pred = cart_model.predict(X_test)
+y_prob = cart_model.predict_proba(X_test)[:, 1]
+print(classification_report(y_test, y_pred))
+roc_auc_score(y_test, y_prob) #modelin perforomansı
+#0.7533928239449712
+
+
+#karar ağacını görselleştirme işlemi
+def tree_graph_to_png(tree, feature_names, png_file_to_save):
+    tree_str = export_graphviz(tree, feature_names=feature_names, filled=True, out_file=None)
+    graph = pydotplus.graph_from_dot_data(tree_str)
+    graph.write_png(png_file_to_save)
+
+tree_graph_to_png(tree=cart_model, feature_names=X_train.columns, png_file_to_save='cart.png')
+
+#karar kuralları çıkarma işlemi
+tree_rules = export_text(cart_model, feature_names=list(X_train.columns))
+print(tree_rules)
+
+
+#değişken önem düzeylerini incelemek
+def plot_importance(model, features, num=len(X), save=False):
+    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
+    plt.figure(figsize=(10, 10))
+    sns.set(font_scale=1)
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
+                                                                     ascending=False)[0:num])
+    plt.title('Features')
+    plt.tight_layout()
+    plt.show()
+    if save:
+        plt.savefig('importances.png')
+
+plot_importance(cart_model, X_train)
+
+cart_model = DecisionTreeClassifier(random_state=17)
+# arama yapılacak hiperparametre setleri
+cart_params = {'max_depth': range(1, 11),
+               "min_samples_split": [2, 3, 4]}
+
+#cross validation ile hiperparemetre araması yapacağız
+#Yani hiperparametre araması yapılırken train seti üzerinde yapılır tüm veri üzerinde yapılmaz!
+cart_cv = GridSearchCV(cart_model, cart_params, cv=5, n_jobs=-1, verbose=True)
+cart_cv.fit(X_train, y_train) #train setini çapraz doğrulamaya sokuyor
+#model doğrulama yapılınca asla bütün veri kullanılmaz !!Yanlılık oluşturur!!!
+
+
+cart_tuned = DecisionTreeClassifier(**cart_cv.best_params_).fit(X_train, y_train)
+#** tüm parametreler
+# train hatası
+y_pred = cart_tuned.predict(X_train)
+y_prob = cart_tuned.predict_proba(X_train)[:, 1]
+print(classification_report(y_train, y_pred))
+roc_auc_score(y_train, y_prob)
+#train setindeki hata :0.9123278443113773
+
+#test  hatası
+y_pred = cart_tuned.predict(X_test)
+y_prob = cart_tuned.predict_proba(X_test)[:, 1]
+print(classification_report(y_test, y_pred))
+roc_auc_score(y_test, y_prob)
+#test hatamız :0.8587562744004462
+
+
+################################
+# FİNAL MODELİN YENİDEN TÜM VERİYE FİT EDİLMESİ
+################################
+
+cart_tuned_final = DecisionTreeClassifier(**cart_cv.best_params_).fit(X, y)
+
+################################
+# MODELİN DAHA SONRA KULLANILMAK ÜZERE KAYDEDİLMESİ
+################################
+
+import joblib
+joblib.dump(cart_tuned_final, "cart_tuned_final.pkl")
+
+cart_model_from_disk = joblib.load("cart_tuned_final.pkl")
+
+cart_model_from_disk.predict(X_test)
+
+
+
 
 
 
