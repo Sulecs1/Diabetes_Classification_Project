bdfb8dc4c4f2de483580688b3e5b9715f27c7b06
Sulecs1 <sule.akcay3@ogr.sakarya.edu.tr>
2021-03-01 11:40:24 +0300

    fonksiyonlar eklendi

--- Diabetes_Classification.py
+++ Diabetes_Classification.py
@@ -56,3 +56,55 @@ plt.show()
 #veri seti gözlemler hakkında inceleme yapıldı
 grab_col_names(df)
 
+def data_understand(df):
+    print("DF SHAPE:", df.shape)
+    print("------------------------------------------------------------------------")
+    print("OUTCOME 1 DF RATIO:", len(df[df["Outcome"] == 1]) / len(df))
+    print("OUTCOME 0 DF RATIO:", len(df[df["Outcome"] == 0]) / len(df))
+    print("------------------------------------TYPES------------------------------------")
+    print(df.dtypes)
+    print("------------------------------------HEAD------------------------------------")
+    print(df.head())
+    print("-------------------------------------TAİL-----------------------------------")
+    print(df.tail())
+    print("------------------------------------DESCRİBE------------------------------------")
+    print(df.describe().T)
+    print("-------------------------------------QUANTİLE-----------------------------------")
+    print(df.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+    print("-----------------------------------CORR-------------------------------------")
+    # Isı haritasında, daha parlak renkler daha fazla korelasyonu gösterir.
+    # Tablodan ve ısı haritasından da görebileceğimiz gibi, glikoz seviyeleri, yaş, vücut kitle indeksi ve gebelik sayısı, sonuç değişkeni ile önemli bir korelasyona sahiptir. Ayrıca yaş ve gebelikler veya insülin ve cilt kalınlığı gibi özellik çiftleri arasındaki korelasyona dikkat ediniz.
+
+    corr = df.corr()
+    print(corr)
+    print("--------------------------------------HEATMAP----------------------------------")
+    sns.heatmap(corr,
+                xticklabels=corr.columns,
+                yticklabels=corr.columns)
+    plt.show()
+    print("------------------------------------------------------------------------")
+    print(sns.pairplot(df));
+    plt.show()
+    print("------------------------------------------------------------------------")
+    df.hist(bins=20, color="#1c0f45", edgecolor='orange', figsize=(15, 15));
+    plt.show()
+    print("------------------------------------------------------------------------")
+
+data_understand(df)
+
+#Veri setindeki eksik değerleri sorgulamak için
+def df_questioning_null(df):
+    """
+    Veri seti içerisindeki eksik değere sahip değişken bilgisini sorgular. Bununla birlikte eksik gözlem olması durumunda sadece eksik gözleme sahip sütun isimlerini getirir.
+    """
+    print(f"Veri kümesinde hiç boş değer var mı?: {df.isnull().values.any()}")
+    if df.isnull().values.any():
+        null_values = df.isnull().sum()
+        print(f"Hangi sütunlarda eksik değerler var?:\n{null_values[null_values > 0]}")
+
+df_questioning_null(df)
+
+
+
+
+
